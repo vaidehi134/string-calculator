5545b637374129f3bb23945c2a02152f576acc4b
vaidehi gondalia <gondaliavaidehi@gmail.com>
2025-07-04 18:45:35 +0530

    test 6: support custom delimiter using //delimeter\n syntax

--- src/main/java/com/incubyte/string_calculator/StringCalculator.java
+++ src/main/java/com/incubyte/string_calculator/StringCalculator.java
@@ -1,11 +1,21 @@
 package com.incubyte.string_calculator;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public int add(String input) {
         if (input.isEmpty()) return 0;
 
-        String[] numbers = input.split(",|\n"); // split on comma OR newline
+        String delimiter = ",|\n"; // default delimiters
+
+        if (input.startsWith("//")) {
+            String[] parts = input.split("\n", 2);
+            delimiter = Pattern.quote(parts[0].substring(2)); // escape special chars
+            input = parts[1];
+        }
+
+        String[] numbers = input.split(delimiter);
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.parseInt(number);
@@ -13,6 +23,7 @@ public class StringCalculator {
 
         return sum;
     }
+
 
 
 
